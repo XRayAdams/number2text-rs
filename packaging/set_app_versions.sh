4ba43d718a3107070550a160c85bafdbc0d35fb5
Konstantin Adamov <xrayadamo@gmail.com>
2026-04-14 12:18:41 -0700

    Moving DEB creation into cargo

--- packaging/set_app_versions.sh
+++ packaging/set_app_versions.sh
@@ -6,10 +6,8 @@ echo "Setting app version in all relevant files..."
 
 # --- Configuration ---
 CARGO_FILE="Cargo.toml"
-DEBIAN_CONTROL_FILE="packaging/control"
 SNAP_YAML_FILE="snap/snapcraft.yaml"
 RPM_FILE="packaging/number2text.spec"
-FEDORA_SPEC_FILE="packaging/rpmpublish/app.rayadams.number2text.spec"
 MACHINE_ARCH=$(uname -m)
 DEBIAN_CONTROL_FILE_ARCH="amd64"
 AUR_CONTROL_FILE="packaging/PKGBUILD"
@@ -30,20 +28,12 @@ if [ ! -f "$CARGO_FILE" ]; then
     echo "Error: File not found: $CARGO_FILE"
     exit 1
 fi
-if [ ! -f "$DEBIAN_CONTROL_FILE" ]; then
-    echo "Error: File not found: $DEBIAN_CONTROL_FILE"
-    exit 1
-fi
 if [ ! -f "$SNAP_YAML_FILE" ]; then
     echo "Error: File not found: $SNAP_YAML_FILE"
     exit 1
 fi
 if [ ! -f "$RPM_FILE" ]; then
     echo "Error: File not found: $RPM_FILE"
-    exit 1
-fi
-if [ ! -f "$FEDORA_SPEC_FILE" ]; then
-    echo "Error: File not found: $FEDORA_SPEC_FILE"
     exit 1
 fi
 
@@ -61,23 +51,9 @@ echo "Version '$APP_VERSION' found in $CARGO_FILE"
 APP_VERSION_SHORT=$(echo "$APP_VERSION" | cut -d'+' -f1)
 APP_BUILD=$(echo "$APP_VERSION" | cut -d'+' -f2)
 
-# Use sed to find and replace the Version line in debian.yaml and snap
-# This command looks for the line starting with '  Version:' and replaces the entire line.
-sed -i "s/^\(\s*Version:\s*\).*\$/\1$APP_VERSION/" "$DEBIAN_CONTROL_FILE"
-sed -i "s/^\(\s*Architecture:\s*\).*\$/\1$DEBIAN_CONTROL_FILE_ARCH/" "$DEBIAN_CONTROL_FILE"
-sed -i "s/^\(\s*version:\s*\).*\$/\1$APP_VERSION/" "$SNAP_YAML_FILE"
-
 # Update version in RPM spec file
 sed -i "s/^\(\s*%define _version \s*\).*\$/\1$APP_VERSION_SHORT/" "$RPM_FILE"
 sed -i "s/^\(\s*%define _release \s*\).*\$/\1$APP_BUILD/" "$RPM_FILE"
-
-# Update version in Fedora spec file
-CHANGELOG_DATE=$(date +"%a %b %d %Y")
-CHANGELOG_VER="$APP_VERSION_SHORT-$APP_BUILD"
-sed -i "s/^\(%global upstreamver \).*\$/\1$APP_VERSION/" "$FEDORA_SPEC_FILE"
-sed -i "s/^\(Version:\s*\).*\$/\1$APP_VERSION_SHORT/" "$FEDORA_SPEC_FILE"
-sed -i "s/^\(Release:\s*\).*\$/\1${APP_BUILD}%{?dist}/" "$FEDORA_SPEC_FILE"
-sed -i "s/^\(\* \).*\(<xrayadamo@gmail\.com>\).*\$/\1$CHANGELOG_DATE Konstantin Adamov \2 - $CHANGELOG_VER/" "$FEDORA_SPEC_FILE"
 
 # Update version in AUR PKGBUILD
 sed -i "s/^\(\s*pkgver=\).*\$/\1$APP_VERSION_SHORT/" "$AUR_CONTROL_FILE"
